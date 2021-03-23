95477cae9aeb1969c05bdc87addc2873850c7874
tanknee <nee@tanknee.cn>
2021-03-23 10:37:01 +0800

    first commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,12 @@
+<template>
+  <q-page class="flex flex-center">
+    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg" />
+    <iframe src="https://ljscq.bmcx.com/"></iframe>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "PageIndex"
+};
+</script>
